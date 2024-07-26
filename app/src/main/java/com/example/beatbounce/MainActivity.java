232e5eec3950fa0424df7d6bfdac4c5ca65a5e8d
SarahWidya17 <127605135+SarahWidya17@users.noreply.github.com>
2024-07-26 08:39:24 +0700

    History

--- app/src/main/java/com/example/beatbounce/MainActivity.java
+++ app/src/main/java/com/example/beatbounce/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.beatbounce;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button favoriteButton = findViewById(R.id.favorite_button_change);
+
+        favoriteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                v.setSelected(!v.isSelected());
+            }
+        });
     }
 }
