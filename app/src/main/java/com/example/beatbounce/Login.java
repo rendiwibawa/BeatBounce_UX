d660db9dfd88906b3bea14649c27c21f3b521676
Ajeng Cipta Wening <ajengcwn@gmail.com>
2024-08-05 09:47:38 +0700

    update gwe lg

---
+++ app/src/main/java/com/example/beatbounce/Login.java
@@ -0,0 +1,20 @@
+package com.example.beatbounce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class Login extends AppCompatActivity {
+
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        login.findViewById(R.id.loginButton);
+
+    }
+}
