4f6669b5c2e5fa2926d2969e580ad854cf9957c8
Rendi prayata wibawa <54298924+rendiwibawa@users.noreply.github.com>
2024-08-05 16:10:44 +0700

    Update booking

---
+++ app/src/main/java/com/example/beatbounce/History/HistoryActivity.java
@@ -0,0 +1,57 @@
+package com.example.beatbounce.History;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.beatbounce.R;
+import com.example.beatbounce.SpaceItemDecoration;
+import com.example.beatbounce.Studio;
+import com.example.beatbounce.StudioAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HistoryActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private StudioAdapter studioAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_history);
+
+        recyclerView = findViewById(R.id.recycler_view_studios);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        studioAdapter = new StudioAdapter(getStudios());
+        recyclerView.setAdapter(studioAdapter);
+
+        int spacing = getResources().getDimensionPixelSize(R.dimen.item_spacing);
+        recyclerView.addItemDecoration(new SpaceItemDecoration(spacing));
+    }
+
+    private List<Studio> getStudios() {
+        List<Studio> studios = new ArrayList<>();
+
+        studios.add(new Studio(
+                "BP Studio Dance - Jazz Dance",
+                R.drawable.studio_dance_jazz,
+                "Bogor",
+                "Rp. 250.000 / jam",
+                4.5f
+        ));
+
+        studios.add(new Studio(
+                "TI Studio Dance for Ballet",
+                R.drawable.studio_dance_ballet,
+                "Jakarta",
+                "Rp. 345.000 / jam",
+                4.8f
+        ));
+
+        return studios;
+    }
+}
