ee4f66844b1fde2b60c9f83ba4dce8ce33c510df
Ajeng Cipta Wening <ajengcwn@gmail.com>
2024-08-08 19:12:50 +0700

    aaaaaaa

---
+++ app/src/main/java/com/example/beatbounce/Login/ChangePasswordActivity.java
@@ -0,0 +1,97 @@
+package com.example.beatbounce.Login;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.beatbounce.R;
+import com.example.beatbounce.databinding.ActivityChangePasswordBinding;
+
+
+public class ChangePasswordActivity extends AppCompatActivity {
+
+    // Membuat variabel binding dengan tipe data ActivityChangePasswordBinding
+    private ActivityChangePasswordBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Menginisialisasi variabel binding dengan memanggil metode inflate() dari ActivityChangePasswordBinding
+        binding = ActivityChangePasswordBinding.inflate(getLayoutInflater());
+        // Menampilkan layout yang diinginkan
+        setContentView(binding.getRoot());
+
+        initView();
+    }
+
+    private void initView() {
+        // Menambahkan aksi klik pada TextView tvForgotPassword
+        binding.tvForgotPassword.setOnClickListener(v -> {
+            // Membuat objek dialog dengan tipe data ResetPasswordDialog
+            ResetPasswordDialog dialog = new ResetPasswordDialog();
+
+            // Menampilkan dialog
+            dialog.show(getSupportFragmentManager(), dialog.getTag());
+        });
+
+        // Menambahkan aksi klik pada Button btnBack
+        binding.btnBack.setOnClickListener(v -> finish()); // Menutup halaman
+
+        // Menambahkan aksi klik pada Button btnSave
+        binding.btnSave.setOnClickListener(v -> {
+            // Mengambil nilai dari EditText etCurrentPassword, etNewPassword, dan etPasswordConfirmation
+            String currentPassword = binding.etCurrentPassword.getText().toString();
+            String newPassword = binding.etNewPassword.getText().toString();
+            String confirmPassword = binding.etPasswordConfirmation.getText().toString();
+
+            // Membuat variabel isValid dengan tipe data boolean dan memberikan nilai true
+            boolean isValid = true;
+
+            // Mengecek apakah nilai dari currentPassword kosong
+            if (currentPassword.isEmpty()) {
+                // Menampilkan pesan error pada EditText etCurrentPassword
+                binding.etCurrentPassword.setError(getString(R.string.error_password_empty));
+
+                // Mengubah nilai isValid menjadi false
+                isValid = false;
+            }
+
+            // Mengecek apakah nilai dari newPassword kosong
+            if (newPassword.isEmpty()) {
+                // Menampilkan pesan error pada EditText etNewPassword
+                binding.etNewPassword.setError(getString(R.string.error_password_empty));
+
+                // Mengubah nilai isValid menjadi false
+                isValid = false;
+            }
+
+            // Mengecek apakah nilai dari confirmPassword kosong
+            if (confirmPassword.isEmpty()) {
+                // Menampilkan pesan error pada EditText etPasswordConfirmation
+                binding.etPasswordConfirmation.setError(getString(R.string.error_password_empty));
+
+                // Mengubah nilai isValid menjadi false
+                isValid = false;
+            }
+
+            // Mengecek apakah nilai dari newPassword tidak sama dengan nilai dari confirmPassword
+            if (!newPassword.equals(confirmPassword)) {
+                // Menampilkan pesan error pada EditText etPasswordConfirmation
+                binding.etPasswordConfirmation.setError(getString(R.string.error_password_not_match));
+
+                // Mengubah nilai isValid menjadi false
+                isValid = false;
+            }
+
+            // Mengecek apakah nilai isValid bernilai true
+            if (isValid) {
+                // Menampilkan pesan sukses
+                Toast.makeText(this, getString(R.string.change_password_success), Toast.LENGTH_SHORT).show();
+
+                // Menutup halaman
+                finish();
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/beatbounce/Login/LoginActivity.java
@@ -0,0 +1,100 @@
+package com.example.beatbounce.Login;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+import android.text.style.UnderlineSpan;
+import android.util.Patterns;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
+import com.example.beatbounce.databinding.ActivityLoginBinding;
+
+
+import com.example.beatbounce.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private ActivityLoginBinding binding;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Hapus baris berikut:
+        // setContentView(R.layout.activity_login);
+
+        binding = ActivityLoginBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        initView();
+    }
+
+
+    private void initView() {
+
+
+        SpannableString spannableString = new SpannableString(getString(R.string.text_register));
+        spannableString.setSpan(new UnderlineSpan(), 18, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new ForegroundColorSpan(ResourcesCompat.getColor(getResources(), R.color.orange, null)), 18, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new StyleSpan(android.graphics.Typeface.BOLD), 18, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        spannableString.setSpan(new CustomTypefaceSpan(ResourcesCompat.getFont(this, R.font.poppins_bold)), 18, spannableString.length(), Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
+        binding.textRegister.setText(spannableString);
+
+        binding.btnLogin.setOnClickListener(this::onClick);
+
+        binding.textRegister.setOnClickListener(v -> {
+            Intent intent = new Intent(this, RegisterActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    private void resetError() {
+        binding.etEmail.setError(null);
+        binding.etPassword.setError(null);
+    }
+
+    private void onClick(View v) {
+        resetError();
+        String email = binding.etEmail.getText().toString();
+        String password = binding.etPassword.getText().toString();
+        boolean isValid = true;
+
+        if (email.isEmpty()) {
+            binding.etEmail.setError(getString(R.string.error_email_empty));
+            isValid = false;
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            binding.etEmail.setError(getString(R.string.error_email_invalid));
+            isValid = false;
+        }
+
+        if (password.isEmpty()) {
+            binding.etPassword.setError(getString(R.string.error_password_empty));
+            isValid = false;
+        }
+
+        if (password.length() < 6) {
+            binding.etPassword.setError(getString(R.string.error_password_short));
+            isValid = false;
+        }
+
+        if (isValid) {
+
+            String[] emailParts = email.split("@gmail.com");
+            if (emailParts.length > 0) {
+                String emailPrefix = emailParts[0];
+
+                // Kirim emailPrefix ke aktivitas lain
+                Intent intent = new Intent(LoginActivity.this, ProfileActivity.class);
+                intent.putExtra("EMAIL_PREFIX", emailPrefix);
+                intent.putExtra("EMAIL", email);
+                startActivity(intent);
+            }
+        }
+    }
+}
